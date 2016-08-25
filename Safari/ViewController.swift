faf33d55bb6554f676d645a307c539a2a9aea724
Komari Herring <komherr@gmail.com>
2016-08-24 21:53:40 -0400

    done

--- Safari/ViewController.swift
+++ Safari/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController, UITextFieldDelegate
+{
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    
+    {
+        
+        let url = URL(string: "https://\(textField.text!)")
+        
+        let request = URLRequest(url: url!)
+        
+        webView.load(request)
+        
+        return true
+        
+    }
+    
+    
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
